686f01dc4a63ff9db6c9cf2244fc30487432ab9e
Furkan <furkan332004@gmail.com>
2026-07-05 15:22:55 +0300

    Karakter animasyonu

--- Assets/Scripts/Elements/Enemy.cs
+++ Assets/Scripts/Elements/Enemy.cs
@@ -7,11 +7,15 @@ public class Enemy : MonoBehaviour
     public float speed;
     private Rigidbody _rb;
     public NavMeshAgent navMeshAgent;
+    private Animator _animator;
+
+    private bool _isWalking = false;
 
     public void StartEnemy(Player player)
     {
         _player = player;
         _rb = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -23,12 +27,20 @@ public class Enemy : MonoBehaviour
             _rb.position += direction * speed * Time.deltaTime;  */
 
             navMeshAgent.SetDestination(_player.transform.position);
+
+            if (!_isWalking)
+            {
+                _animator.SetTrigger("Walk");
+                _isWalking = true;
+            }
+
         }
     }
 
     public void StopMovement()
     {
         navMeshAgent.speed = 0;
+        _animator.SetTrigger("Idle");
     }   
 
 }
